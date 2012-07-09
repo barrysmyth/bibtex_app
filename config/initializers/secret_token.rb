870ba72831d4ce47bf091e953832c7633cc548ad
Barry Smyth <barry.smyth@gmail.com>
2012-07-09 09:59:03 +0100

    Initi commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BibtexApp::Application.config.secret_token = '1ef61073ba4cb8f5f08c94f76e9ead4bab57aefe658e1a52ac103c9256e572e09907594ac2346166831caa03d397ce281c2c5e597f13ca89557995eac517191c'
